99e384f4276254bad54cd3c46dddc0e18e260fc2
“sk07103“ <shurit400@gmail.com>
2023-01-13 13:29:17 +0900

    forms.pyを追加

---
+++ accounts/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import User
+from django.contrib.auth.password_validation import validate_password
+
+class CreateUserForm(forms.ModelForm):
+
+    class Meta:
+        model = User        
+        fields = ['username', 'email', 'picture', 'password']
+        labels = {
+            'username': 'ユーザー名',
+            'email': 'メールアドレス',
+            'picture': '画像',
+            'password': 'パスワード',
+        }
+        widgets = {
+            'password': forms.PasswordInput()
+        }
+        requireds = {
+            'picture': False
+        }
+
+    def save(self, commit=False):
+      user = super().save(commit=False)
+      validate_password(self.cleaned_data['password'], user)
+      user.set_password(self.cleaned_data['password'])
+      user.save()
+      return user
+
