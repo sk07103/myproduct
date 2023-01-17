599614c9b064dc8707b7fd4f28cedf61ff38a9fe
sk07103 <shurit400@gmail.com>
2023-01-17 22:44:23 +0900

    urlpatternsを追加（user_create）

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import CreateUserView
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('user_create/', CreateUserView.as_view(), name='user_create')
 ]
