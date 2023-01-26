b9a466acdaa51bedd2951527291bdc40da572013
“sk07103“ <shurit400@gmail.com>
2023-01-26 14:33:19 +0900

    login用のpathを追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CreateUserView
+
+from .views import CreateUserView, LoginUserView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('user_create/', CreateUserView.as_view(), name='user_create')
+    path('user_create/', CreateUserView.as_view(), name='user_create'),
+    path('user_login/', LoginUserView.as_view(), name='user_login'),
 ]
