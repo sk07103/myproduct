ce8612d04ea439fff0fb1690f3675fcdbde240d1
“sk07103“ <shurit400@gmail.com>
2023-01-26 14:09:18 +0900

    login用のviewを作成

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,26 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
-from .forms import RegistUserForm
+
+from .forms import RegistUserForm, LoginUserForm
 
 class CreateUserView(CreateView):
+
     template_name = 'accounts/user_regist.html'
     form_class = RegistUserForm
 
+
+class LoginUserView(LoginView):
+
+    template_name = 'accounts/user_login.html'
+    authentication_form = LoginUserForm
+
+    def form_valid(self, form):
+        remember = form.cleaned_data['remember']
+        if remember:
+            self.request.session.set_expiry(3600)
+        return super().form_valid(form)
+
+
+class UserLogoutView(LogoutView):
+
+    pass
