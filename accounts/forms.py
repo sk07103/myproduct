1f9aa5c069afa0e14c153978781503e3f65c1461
“sk07103“ <shurit400@gmail.com>
2023-01-26 14:03:03 +0900

    login用のフォームを作成

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,14 @@
 from datetime import date
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.password_validation import validate_password
 
 from .models import User
 
 class RegistUserForm(forms.ModelForm):
-    
+
     skin_type = forms.ChoiceField(label='ご自身の肌タイプ', choices=User.SKIN_TYPE_CHOICES, widget=forms.RadioSelect())
-    
+
     class Meta:
         model = User
         fields = ['username', 'email', 'date_of_birth', 'skin_type', 'picture', 'password']
@@ -29,9 +30,16 @@ class RegistUserForm(forms.ModelForm):
             }
 
     def save(self, commit=False):
-      user = super().save(commit=False)
-      validate_password(self.cleaned_data['password'], user)
-      user.set_password(self.cleaned_data['password'])
-      user.save()
-      return user
+        user = super().save(commit=False)
+        validate_password(self.cleaned_data['password'], user)
+        user.set_password(self.cleaned_data['password'])
+        user.save()
+        return user
+
+
+class LoginUserForm(AuthenticationForm):
+
+    username = forms.EmailField(label='メールアドレス')
+    password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
+    remember = forms.BooleanField(label='ログイン状態を保持する', required=False)
 
