83f05099981fa82d3453669165e9ea35df2ed5ea
sk07103 <shurit400@gmail.com>
2023-03-27 18:04:28 +0900

    新しく定義したテーブルを追加

--- favoritecosme/admin.py
+++ favoritecosme/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import MyItems
+from .models import MyItems, Categories, Brands, Reviews
 
 admin.site.register(MyItems)
-
+admin.site.register(Categories)
+admin.site.register(Brands)
+admin.site.register(Reviews)
