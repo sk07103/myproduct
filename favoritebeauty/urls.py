4f64f95aaccf7fb2dfbb0be57b238c544edf086f
sk07103 <shurit400@gmail.com>
2023-04-02 23:38:06 +0900

    アイテム一覧ページを作成作成&ユーザー詳細画面と統合

--- favoritebeauty/urls.py
+++ favoritebeauty/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TopView, RegistMyitemView
+from .views import TopView, RegistMyitemView, HomeView
 
 app_name = 'favoritebeauty'
 
 urlpatterns = [
-  path('regist_myitem/', RegistMyitemView.as_view(), name='regist_myitem'),
-  path('top/', TopView.as_view(), name='top'),
+    path('top/', TopView.as_view(), name='top'),
+    path('regist_myitem/', RegistMyitemView.as_view(), name='regist_myitem'),
+    path('home/', HomeView.as_view(), name='home'),
 ]
--- favoritebeauty/views.py
+++ favoritebeauty/views.py
@@ -1,8 +1,9 @@
 import logging
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.views.generic.base import TemplateView
 
 from .forms import RegistMyitemForm
@@ -16,7 +17,7 @@ class TopView(TemplateView):
     template_name = 'favoritebeauty/top.html'
 
 
-class RegistMyitemView(CreateView):
+class RegistMyitemView(LoginRequiredMixin, CreateView):
 
     template_name = 'favoritebeauty/regist_myitem.html'
     form_class = RegistMyitemForm
@@ -29,3 +30,18 @@ class RegistMyitemView(CreateView):
         data.save()
         return super().form_valid(form)
 
+
+class HomeView(LoginRequiredMixin, ListView):
+
+    template_name = 'favoritebeauty/home.html'
+    model = MyItems
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['user'] = self.request.user
+        return context
+
+    def get_queryset(self, **kwargs):
+        queryset = super().get_queryset(**kwargs)
+        queryset = queryset.filter(user=self.request.user)
+        return queryset
