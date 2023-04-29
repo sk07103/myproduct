70418dbec7273af3d42ec9c7da73d3b8bef2e84b
“sk07103“ <shurit400@gmail.com>
2023-04-29 21:05:48 +0900

    メール送信用のカスタムモジュールを追加

--- custom_modules/module.py
+++ custom_modules/module.py
@@ -1,3 +1,14 @@
+import logging
+from django.core.mail import EmailMultiAlternatives
+
+logger = logging.getLogger('file')
+
+# Amazon SESを使用してメールを送信
+def send_email(subject, text_content, from_email, to_emails):
+    logger.info('send_emailが呼び出されました。')
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to_emails])
+    msg.send()
+
 # requestされたurlのidとログイン中ユーザーのuser_idが一致していることをチェック
 def check_userid(request, user_id):
     check_result = True
