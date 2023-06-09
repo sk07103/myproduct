7601f046d08eb736938a9684f1e73fc89bdaec05
“sk07103“ <shurit400@gmail.com>
2023-06-09 16:21:31 +0900

    定期実行を無効化

--- favoritebeauty/apps.py
+++ favoritebeauty/apps.py
@@ -5,6 +5,6 @@ class FavoritebeautyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'favoritebeauty'
 
-    def ready(self):
-        from .ap_scheduler import start
-        start()
+    #def ready(self):
+    #    from .ap_scheduler import start
+    #    start()
